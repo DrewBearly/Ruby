6f75d1db35e1b68a0d1a4617abb746dce1d3fd91
Andrew Herrold <drewherrold@gmail.com>
2019-05-03 07:50:46 -0700

    First Commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Ruby
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
